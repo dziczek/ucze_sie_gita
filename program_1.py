8c60dc7a4545622e2ed73b31df73a08d80fa2006
dziczek <monika.dzik@gmail.com>
2022-03-18 21:45:18 +0100

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -4,6 +4,10 @@ print("helloo, helloo")
 print('Success!!!')
 print("ok, push-checking")
 
+print("Now lets try a pull")
+
 # PUSH
 # 1.Save 2.Changes -> appears a plus + -> click on that plus 3.Click on the v sign ("ptaszek") and add a comment 4.on the left side of "ptaszek" there are two arrows (going up and down) they mean "push" -> click push and  OK appears -> click OK
 
+# PULL
+# 1.Go to file 2.Click on the pencil a the right upper corner 3.Edit the code 4.Click on tje green button below "commit changes" 
