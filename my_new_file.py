eb99b680e51109dcd9ae9c4f471f7916c3dd20de
dziczek <monika.dzik@gmail.com>
2022-03-18 20:31:10 +0000

    added another line

--- my_new_file.py
+++ my_new_file.py
@@ -7,5 +7,6 @@ print("I've forgot almost all")
 print("So far - nothing")
 print("Another try")
 print("After an hour it works. So far. Let's try again")
+print("checking notes")
 
-# 1.Save 2. Changes -> appear a plus +
+# 1.Save 2. Changes -> appear a plus + click on that plus 3. Click on the v sign ('ptaszek") and add a comment 4. on the left side of "ptaszek" there are two arrows (going up and down) they men "push" -> click push
