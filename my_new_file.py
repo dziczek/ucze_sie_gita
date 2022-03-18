5b856f21cf82dfcf0dbb8289aab9f93de73ce661
dziczek <monika.dzik@gmail.com>
2022-03-18 20:28:08 +0000

    Added another line

--- my_new_file.py
+++ my_new_file.py
@@ -6,3 +6,6 @@ print("Great, it works")
 print("I've forgot almost all")
 print("So far - nothing")
 print("Another try")
+print("After an hour it works. So far. Let's try again")
+
+# 1.Save 2. Changes -> appear a plus +
