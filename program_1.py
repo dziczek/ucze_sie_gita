8587f4434b2b9956b710c5cb293ac1f5a24de6b0
dziczek <monika.dzik@gmail.com>
2022-03-18 21:49:36 +0100

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -5,6 +5,7 @@ print('Success!!!')
 print("ok, push-checking")
 
 print("Now lets try a pull")
+print("Pull checking")
 
 # PUSH
 # 1.Save 2.Changes -> appears a plus + -> click on that plus 3.Click on the v sign ("ptaszek") and add a comment 4.on the left side of "ptaszek" there are two arrows (going up and down) they mean "push" -> click push and  OK appears -> click OK
