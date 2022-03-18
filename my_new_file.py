9ec4107456cec3b91a48d99c4f1ebf58443843cb
dziczek <monika.dzik@gmail.com>
2022-03-18 20:36:53 +0000

    checked notes

--- my_new_file.py
+++ my_new_file.py
@@ -10,4 +10,4 @@ print("After an hour it works. So far. Let's try again")
 print("checking notes")
 print("verifying notes")
 
-# 1.Save 2. Changes -> appear a plus + click on that plus 3. Click on the v sign ("ptaszek") and add a comment 4. on the left side of "ptaszek" there are two arrows (going up and down) they maen "push" -> click push and  OK appears -> click OK
+# 1.Save 2.Changes -> appears a plus + -> click on that plus 3.Click on the v sign ("ptaszek") and add a comment 4.on the left side of "ptaszek" there are two arrows (going up and down) they mean "push" -> click push and  OK appears -> click OK
