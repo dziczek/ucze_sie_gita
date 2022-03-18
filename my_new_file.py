b9ca2b7289657dc4146c14390a9cee93a871ea38
dziczek <monika.dzik@gmail.com>
2022-03-18 20:51:06 +0000

    added comment about pull

--- my_new_file.py
+++ my_new_file.py
@@ -12,3 +12,6 @@ print("verifying notes")
 
 # PUSH
 # 1.Save 2.Changes -> appears a plus + -> click on that plus 3.Click on the v sign ("ptaszek") and add a comment 4.on the left side of "ptaszek" there are two arrows (going up and down) they mean "push" -> click push and  OK appears -> click OK
+
+# PULL
+# 1.Go to file 2.Click on the pencil a the right upper corner 3.Edit the code 4.Click on tje green button below "commit changes" 5.Open the file in VSC 6.click on the third icon from the top (source control) 7.click the ring made of two arrows
